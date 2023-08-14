6b11b1ae688d4abb7beff1ff9ffe0214ca9cf7f6
J <jpham26@gmail.com>
2023-08-13 17:01:44 -0700

    Clean up code

--- javascript.js
+++ javascript.js
@@ -10,15 +10,14 @@ const toggleSignButton = document.getElementById('btn-sign');
 let currentValue = null;
 let previousValue = null;
 let operator = null;
-let lastOperator = null;
-let lastOperand = null;
-let calculationResult = null;
-let continuedCalculation = false;
-
-
+let continueCalculationOperator = null;
+let continueCalculationOperand = null;
+let continueCalculationResult = null;
+let continueCalculation = false;
 
 
 function updateDisplay() {
+  // Update font size to fit display 
   if (currentValue.length <= 9) {
     display.style.fontSize = '42px';
   } else if (currentValue.length < 11) {
@@ -29,21 +28,24 @@ function updateDisplay() {
     display.style.fontSize = '24px';
   } else if (currentValue.length <= 20) {
     display.style.fontSize = '18px';
-  } 
+  }
 
   display.textContent = currentValue;
 }
+
 
 function roundAccurately(num, places) {
   return parseFloat(Math.round(num + 'e' + places) + 'e-' + places);
 }
 
+
 function clear() {
+  // Reset all data and start fresh
   previousValue = null;
   currentValue = null;
   operator = null;
-  lastOperator = null;
-  calculationResult = null;
+  continueCalculationOperator = null;
+  continueCalculationResult = null;
   display.style.fontSize = '42px';
   display.textContent = '0';
 }
@@ -62,162 +64,156 @@ function convertToPercent() {
 
 
 function handleOperatorClick(input) {
-  if (!continuedCalculation) {
+  if (!continueCalculation) {
+
     if (!previousValue) {
       previousValue = currentValue;
-      lastOperand = currentValue;
+      continueCalculationOperand = currentValue;
     } else if (previousValue) {
-      lastOperand = currentValue;
+      continueCalculationOperand = currentValue;
       currentValue = performCalculation(operator);
       updateDisplay();
       previousValue = currentValue;
     }
+
     operator = input;
-    lastOperator = operator;
-    lastOperand = currentValue;
+    continueCalculationOperator = operator;
+    continueCalculationOperand = currentValue;
     currentValue = null;
-  } else if (continuedCalculation && !operator) {
-    previousValue = calculationResult;
+
+  } else if (continueCalculation && !operator) {
+    previousValue = continueCalculationResult;
     operator = input;
-    lastOperator = operator;
-    lastOperand = currentValue;
-  } else if (continuedCalculation && operator) {
+    continueCalculationOperator = operator;
+    continueCalculationOperand = currentValue;
+
+  } else if (continueCalculation && operator) {
     currentValue = performCalculation(operator);
     updateDisplay();
     previousValue = currentValue;
     operator = input;
-    lastOperator = operator;
+    continueCalculationOperator = operator;
     currentValue = null;
-    lastOperand = currentValue;
+    continueCalculationOperand = currentValue;
   }
 }
+
 
 function countDigits(str) {
   if (str === null) {
     return 0;
+
   } else {
     let digits = str.match(/\d/g);
-    return digits.length;    
+    return digits.length;
   }
 }
 
 
 function handleNumberClick(button) {
+  let currentDigits = countDigits(currentValue);
+
+  // Reset if number clicked after equal sign
   if (operator === null) {
-    continuedCalculation = false;
+    continueCalculation = false;
   }
 
-  let currentDigits = countDigits(currentValue);
-  
+
   if (currentValue === null && button !== '.') {
     currentValue = button;
-    lastOperand = currentValue;
+    continueCalculationOperand = currentValue;
+
   } else if (button === '.' && currentValue.includes('.')) {
     return;
+
   } else if (currentDigits >= 20) {
     return;
+
   } else {
     currentValue += button;
-    lastOperand = currentValue;
-    console.log(typeof(currentValue));
-    console.log(currentValue);
-    console.log(currentValue.length);
+    continueCalculationOperand = currentValue;
   }
+
   updateDisplay();
 }
 
 
 function handleEqualClick() {
-  // console.log(``);  
-  // console.log(`BEFORE:`);  
-  // console.log(`currentValue ${currentValue}`);
-  // console.log(`previousValue ${previousValue}`);
-  // console.log(`operator ${operator}`);
-  // console.log(`lastOperator ${lastOperator}`);
-  // console.log(`lastOperand ${lastOperand}`);
-  // console.log(`calculationResult ${calculationResult}`);
-  // console.log(`continuedCalculation ${continuedCalculation}`);
-  // console.log(``);  
-  if (currentValue && previousValue && !continuedCalculation) {
-    console.log(`equal 1st condition`)
-    result = performCalculation(operator);
-    currentValue = roundAccurately(result,8).toString();
-    calculationResult = currentValue;
-    continuedCalculation = true;
-    updateDisplay();
-    currentValue = null;
-    lastOperator = operator;
-    operator = null; 
-    previousValue = null; 
-  
-  } else if (currentValue && previousValue && continuedCalculation && lastOperand) {
-    console.log(`equal 2-A condition`)
-    currentValue = lastOperand;
-    previousValue = calculationResult;
-    result = performCalculation(operator);
-    currentValue = roundAccurately(result,8).toString();
-    calculationResult = currentValue;
-    continuedCalculation = true;
-    updateDisplay();
-    currentValue = null;
-    lastOperator = operator;
-    operator = null; 
-    previousValue = null; 
 
-  } else if (currentValue && previousValue && continuedCalculation && !lastOperand) {
-    console.log(`equal 2-B condition`)
-    previousValue = calculationResult;
-    result = performCalculation(operator);
-    currentValue = roundAccurately(result,8).toString();
-    calculationResult = currentValue;
-    continuedCalculation = true;
-    updateDisplay();
-    currentValue = lastOperand;
-    lastOperator = operator;
-    lastOperand = currentValue;
-    operator = null; 
-    previousValue = null; 
-
-    // This works once
-  } else if (currentValue === null && previousValue) {
-    console.log(`equal 3rd condition`)
-    currentValue = previousValue;
-    result = performCalculation(operator);
-    currentValue = roundAccurately(result,8).toString();
-    calculationResult = currentValue;
-    lastOperator = operator;
-    lastOperand = previousValue;
-    continuedCalculation === true;
-    updateDisplay(); 
-
-    // Delete this if issues
-  } else if (continuedCalculation && lastOperator) {
-    console.log(`equal 4th condition`)
-    operator = lastOperator;
-    previousValue = calculationResult;
-    currentValue = lastOperand;
+  // Super complex if else statements to handle continuing calculations
+  if (currentValue && previousValue && !continueCalculation) {
     result = performCalculation(operator);
     currentValue = roundAccurately(result, 8).toString();
-    calculationResult = currentValue;
-    lastOperator = operator;
-    continuedCalculation === true;
-    updateDisplay(); 
+    continueCalculationResult = currentValue;
+    continueCalculation = true;
+    updateDisplay();
+    currentValue = null;
+    continueCalculationOperator = operator;
+    operator = null;
+    previousValue = null;
 
+  } else if (
+              currentValue 
+              && previousValue 
+              && continueCalculation 
+              && continueCalculationOperand
+    ) {
+    currentValue = continueCalculationOperand;
+    previousValue = continueCalculationResult;
+    result = performCalculation(operator);
+    currentValue = roundAccurately(result, 8).toString();
+    continueCalculationResult = currentValue;
+    continueCalculation = true;
+    updateDisplay();
+    currentValue = null;
+    continueCalculationOperator = operator;
+    operator = null;
+    previousValue = null;
+
+  } else if (
+              currentValue 
+              && previousValue 
+              && continueCalculation 
+              && !continueCalculationOperand
+    ) {
+    previousValue = continueCalculationResult;
+    result = performCalculation(operator);
+    currentValue = roundAccurately(result, 8).toString();
+    continueCalculationResult = currentValue;
+    continueCalculation = true;
+    updateDisplay();
+    currentValue = continueCalculationOperand;
+    continueCalculationOperator = operator;
+    continueCalculationOperand = currentValue;
+    operator = null;
+    previousValue = null;
+
+  } else if (currentValue === null && previousValue) {
+    currentValue = previousValue;
+    result = performCalculation(operator);
+    currentValue = roundAccurately(result, 8).toString();
+    continueCalculationResult = currentValue;
+    continueCalculationOperator = operator;
+    continueCalculationOperand = previousValue;
+    continueCalculation === true;
+    updateDisplay();
+
+  } else if (continueCalculation && continueCalculationOperator) {
+    operator = continueCalculationOperator;
+    previousValue = continueCalculationResult;
+    currentValue = continueCalculationOperand;
+    result = performCalculation(operator);
+    currentValue = roundAccurately(result, 8).toString();
+    continueCalculationResult = currentValue;
+    continueCalculationOperator = operator;
+    continueCalculation === true;
+    updateDisplay();
+
+    // Disable equal button
   } else {
-    console.log(`equal 3rd condition`)
     return;
   }
-  // console.log(``);  
-  // console.log(`AFTER:`);  
-  // console.log(`currentValue ${currentValue}`);
-  // console.log(`previousValue ${previousValue}`);
-  // console.log(`operator ${operator}`);
-  // console.log(`lastOperator ${lastOperator}`);
-  // console.log(`lastOperand ${lastOperand}`);
-  // console.log(`calculationResult ${calculationResult}`);
-  // console.log(`continuedCalculation ${continuedCalculation}`);
-  // console.log(``);  
-}  
+}
 
 
 function performCalculation(operator) {
